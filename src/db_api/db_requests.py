bf9b49429e45ac50ee72ad6b9c5990883a17d225
kolyazin <kolyazin.se@gmai.com>
2023-10-23 02:57:06 +0300

    added work with db sqlite3

--- src/db_api/db_requests.py
+++ src/db_api/db_requests.py
@@ -9,7 +9,6 @@ class Database:
     def connection(self):
         return sqlite3.connect(self.path_db)
 
-
     def execute(self, sql, parametrs: tuple = tuple(), fetchone=False, fetchall=False, commit=False):
         connection = self.connection
         cursor = connection.cursor()
@@ -21,4 +20,45 @@ class Database:
             data = cursor.fetchone()
         if fetchall:
             data = cursor.fetchall()
+        connection.close()
         return data
+
+    def create_table_users(self):
+        sql = """
+        CREATE TABLE Users(
+        id INT NOT NULL,
+        phone TEXT,
+        PRIMARY KEY (id)
+        );
+        """
+        self.execute(sql, commit=True)
+
+    def add_user(self, id: int, phone: str = None):
+        sql = 'INSERT INTO Users(id, phone) VALUES(?, ?)'
+        parameters = (id, phone)
+        self.execute(sql, parameters, commit=True)
+
+    def select_user_info(self, **kwargs) -> list:
+        sql = 'SELECT * FROM Users WHERE '
+        sql, parameters = self.format_args(sql, kwargs)
+        return self.execute(sql, parameters, fetchall=True)
+
+    def select_all_users(self) -> list:
+        sql = 'SELECT * FROM Users'
+        return self.execute(sql, fetchall=True)
+
+    def delete_user(self, **kwargs):
+        sql = 'DELETE FROM Users WHERE '
+        sql, parameters = self.format_args(sql, parameters=kwargs)
+        return self.execute(sql, parameters=parameters, commit=True)
+
+    def delete_all(self):
+        self.execute('DELETE FROM Users WHERE True', commit=True)
+
+    def drop_all(self):
+        self.execute('DROP TABLE Users', commit=True)
+
+    @staticmethod
+    def format_args(sql, parameters: dict) -> tuple:
+        sql += ' AND '.join([f'{item} = ?' for item in parameters])
+        return sql, tuple(parameters.values())
--- src/loader.py
+++ src/loader.py
@@ -1,7 +1,20 @@
 # инициализация бота
+import sqlite3
+
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
+from db_api import Database
+from pathlib import Path
 
 bot = Bot(BOT_TOKEN)
 # инициализация диспетчера
 dp = Dispatcher(bot)
+db_path = Path('db_api', 'database', 'shop_database.db')
+db = Database(db_path=db_path)
+
+try:
+    db.create_table_users()
+except sqlite3.OperationalError as e:
+    print(e)
+except Exception as e:
+    print(e)
